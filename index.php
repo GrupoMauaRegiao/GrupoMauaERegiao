dce68e8f5df17a68d5c237d3fd784a46046adbcf
Marcos Garcia <marckfree@gmail.com>
2013-04-09 16:45:50 -0300

    Adicionando estrutura HTML básica

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="keywords" content="compra, maua e regiao, compra coletiva, ribeirao pires, rio grande da serra, diadema, suzano, lancamento maua e regiao, grupo maua e regiao, revista maua e regiao, jornal maua e regiao, portal maua e regiao, loja virtual maua, grupo maua e regiao de comunicacao" />
+    <meta name="description" content="Grupo Mauá e Região de Comunicação. Inovar é o que nos move!" />
+    <meta name="author" content="Grupo Mauá e Região de Comunicação" />
+    <link rel="shortcut icon" href="imagens/favicon.ico" />
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Lato:300" />
+    <link rel="stylesheet" href="css/styles.min.css" />
+    <title>Grupo Mauá e Região &#8212; Inovar é o que nos move!</title>
+  </head>
+  <body>
+    <div id="pagina">
+
+    </div> <!-- #pagina -->
+
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <script src="js/scripts.js"></script>
+  </body>
+</html>
