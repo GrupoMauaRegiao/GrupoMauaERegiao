56c83de0a76146bcf9252fcdcaf63d9db0f88bfc
Marcos Garcia <marckfree@gmail.com>
2013-04-11 13:34:48 -0300

    Adicionando formatação ao container do mapa

--- contato.php
+++ contato.php
@@ -37,7 +37,14 @@
             </div>
 
             <div class="endereco">
-
+              <p>
+                Rua Clodoaldo Portugal Caribé, 276 <br />
+                Vila Assis Brasil - Mauá - SP. <br />
+                CEP: 09370-620
+              </p>
+              <p class="maior">
+                <span>+55 (11)</span> 4513-1591
+              </p>
             </div>
           </div>
         </div> <!-- #Direita -->
