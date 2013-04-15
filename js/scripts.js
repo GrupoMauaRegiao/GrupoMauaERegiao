5179b7732751663203ac67c69d0896f2ec9e1456
Marcos Garcia <marckfree@gmail.com>
2013-04-15 13:48:47 -0300

    Alterando design pattern das functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,77 +1,81 @@
-"use strict";
 $(document).ready(function () {
+  "use strict";
+  var grupoMaua = {};
+  grupoMaua.functions = (function () {
+    return {
 
-  function efeitoEntrada() {
-    var pagina;
-    pagina = $('#pagina');
+      efeitoEntrada: function () {
+        var pagina = $('#pagina');
+        setTimeout(function () {
+          pagina.show(1000, 'easeInOutElastic');
+        }, 1);
+      },
 
-    setTimeout(function () {
-      pagina.show(1000, 'easeInOutElastic');
-    }, 1);
-  }
+      animacaoBackground: function () {
+        var body, coords;
+        body = $('body');
 
-  function animacaoBackground() {
-    var body, coords;
-    body = $('body');
+        if (body.attr('class') !== 'interna') {
+          coords = {};
+          coords.x = function () {
+            return Math.floor(Math.random() * 50);
+          };
+          coords.y = function () {
+            return Math.floor(Math.random() * 10);
+          };
 
-    if (body.attr('class') !== 'interna') {
-      coords = {};
-      coords.x = function () {
-        return Math.floor(Math.random() * 50);
-      };
-      coords.y = function () {
-        return Math.floor(Math.random() * 10);
-      };
+          setInterval(function () {
+            body.css('background-position', '0,' + coords.x() + 'px ' + coords.y() + '%');
+          }, 500);
+        }
+      },
 
-      setInterval(function () {
-        body.css('background-position', '0,' + coords.x() + 'px ' + coords.y() + '%');
-      }, 500);
-    }
-  }
+      criarSlidesEmpresas: function () {
+        if (typeof informacoes !== 'undefined') {
+          var json, empresas, html, i, len, slidesContainer;
+          json = informacoes.empresas; // empresas.js
+          empresas = "";
+          html = [];
+          slidesContainer = $('.apresentacao-slides');
 
-  function criarSlidesEmpresas() {
-    if (typeof informacoes !== 'undefined') {
-      var json, empresas, html, i, len, slidesContainer;
-      json = informacoes.empresas; // empresas.js
-      empresas = "";
-      html = [];
-      slidesContainer = $('.apresentacao-slides');
+          for (i = 0, len = json.length; i < len; i += 1) {
+            html[i] =
+              "<a href='" + informacoes.empresas[i].url + "' target='_blank'>" +
+                "<div class='empresa'>" +
+                  "<div class='imagem'>" +
+                    "<img src='imagens/empresas/" + informacoes.empresas[i].imagem + "' alt='' />" +
+                  "</div>" +
+                  "<div class='nome-empresa'>" +
+                    "<p>" + informacoes.empresas[i].empresa.replace(/(^\w+)/, '<span>$1</span>') + "</p>" +
+                  "</div>" +
+                "</div>" +
+              "</a>";
+            empresas += html[i];
+          }
+          slidesContainer.html(empresas);
+        }
+      },
 
-      for (i = 0, len = json.length; i < len; i += 1) {
-        html[i] =
-          "<a href='" + informacoes.empresas[i].url + "' target='_blank'>" +
-            "<div class='empresa'>" +
-              "<div class='imagem'>" +
-                "<img src='imagens/empresas/" + informacoes.empresas[i].imagem + "' alt='' />" +
-              "</div>" +
-              "<div class='nome-empresa'>" +
-                "<p>" + informacoes.empresas[i].empresa.replace(/(^\w+)/, '<span>$1</span>') + "</p>" +
-              "</div>" +
-            "</div>" +
-          "</a>";
-        empresas += html[i];
+      ativarCycle: function () {
+        if ($('.apresentacao-slides').length > 0) {
+          $('.apresentacao-slides').cycle({
+            fx: 'shuffle',
+            easing: 'easeOutBack',
+            timeout: 5000,
+            pause: 1,
+            speed: 500,
+            prev: '.anterior',
+            next: '.proximo'
+          });
+        }
       }
-      slidesContainer.html(empresas);
-    }
-  }
 
-  function ativarCycle() {
-    if ($('.apresentacao-slides').length > 0) {
-      $('.apresentacao-slides').cycle({
-        fx: 'shuffle',
-        easing: 'easeOutBack',
-        timeout: 5000,
-        pause: 1,
-        speed: 500,
-        prev: '.anterior',
-        next: '.proximo'
-      });
-    }
-  }
+    };
+  }());
 
-  efeitoEntrada();
-  animacaoBackground();
-  criarSlidesEmpresas();
-  ativarCycle();
+  grupoMaua.functions.efeitoEntrada();
+  grupoMaua.functions.animacaoBackground();
+  grupoMaua.functions.criarSlidesEmpresas();
+  grupoMaua.functions.ativarCycle();
 
 });
