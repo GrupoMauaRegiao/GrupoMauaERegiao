a4b17e485728ef378ca593eb3c3d93a5ceb0cc38
Marcos Garcia <marckfree@gmail.com>
2013-04-15 10:46:49 -0300

    Implementando alterações no layout e google code para tracking

---
+++ 403.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="keywords" content="compra, maua e regiao, compra coletiva, ribeirao pires, rio grande da serra, diadema, suzano, lancamento maua e regiao, grupo maua e regiao, revista maua e regiao, jornal maua e regiao, portal maua e regiao, loja virtual maua" />
+    <meta name="description" content="Grupo Mauá e Região de Comunicação. Inovar é o que nos move!" />
+    <meta name="author" content="Grupo Mauá e Região" />
+    <link rel="shortcut icon" href="imagens/favicon.ico" />
+    <title>Grupo Mauá e Região de Comunicação</title>
+  </head>
+  <body>
+    Você não tem permissão para acessar. <br />
+    Acesse ===> <a href="http://grupomauaeregiao.com.br">Grupo Mauá e Região de Comunicação</a>
+  </body>
+</html>
