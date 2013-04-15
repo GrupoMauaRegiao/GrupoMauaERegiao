9a1f05ba969e244a9068d3202e2392b0becaedac
Marcos Garcia <marckfree@gmail.com>
2013-04-15 13:55:26 -0300

    Melhorando indentação do arquivo empresas.js

--- js/empresas.js
+++ js/empresas.js
@@ -1,5 +1,21 @@
-var informacoes = { "empresas": [
-  {"empresa": "Portal Mauá e Região", "imagem": "portal.jpg", "url": "http://portalmauaeregiao.com.br"},
-  {"empresa": "Revista Mauá e Região", "imagem": "revista.jpg", "url": "http://revistamaua.com.br"},
-  {"empresa": "Jornal Mauá e Região", "imagem": "jornal.jpg", "url": "http://jornalmauaeregiao.com.br"}
-]};
+var informacoes = {
+  "empresas": [
+    {
+      "empresa": "Portal Mauá e Região",
+      "imagem": "portal.jpg",
+      "url": "http://portalmauaeregiao.com.br"
+    },
+
+    {
+      "empresa": "Revista Mauá e Região",
+      "imagem": "revista.jpg",
+      "url": "http://revistamaua.com.br"
+    },
+
+    {
+      "empresa": "Jornal Mauá e Região",
+      "imagem": "jornal.jpg",
+      "url": "http://jornalmauaeregiao.com.br"
+    }
+  ]
+};
